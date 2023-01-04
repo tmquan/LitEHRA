ae14b38e87da4f26edda77e97000407cb8c45d48
QuanTM <qtran@talosix.com>
2023-01-04 10:58:58 +0700

    Add layout for AI modules

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Lightning Serve for EHR Analytics
 ```
 lightning run app home.py
 ```
+
+
+![alt text](images/tasks.png "Domain - Task - Subtasks")
--- home.py
+++ home.py
@@ -3,12 +3,49 @@ import lightning as L
 import lightning.app.frontend as frontend
 import streamlit as st
 
-def hello_world_streamlit_app(lightning_app_state):
-    st.write('Hello World')
+import os
+import glob 
+import logging
+
+TASK_DIR = "tasks"
+
+def parse_dir(directory):
+    parent, folder = os.path.split(directory)
+    listings = {
+        "root": folder, 
+        "dirs": [],
+        "files": [],
+    }
+    children = os.listdir(directory)
+    for child in children:
+        child_path = os.path.join(directory, child)
+        if os.path.isdir(child_path):
+            listings['dirs'] += [parse_dir( child_path )] # Recursively parsing
+        else:
+            listings['files'] += [child]
+    return listings
+
+
+def home_app(lightning_app_state):
+    # Glob the tasks
+    listings = parse_dir(TASK_DIR)
+
+    # st.sidebar.header("Navigation")
+    with st.sidebar:
+        st.title("Navigation")
+        for domain in listings["dirs"]:
+            st.header("\t"+domain["root"].replace("_", " ").upper())
+            for task in domain["dirs"]:
+                # st.subheader("\t\t"+task["root"].replace("_", " ").upper())
+                with st.expander("\t\t"+task["root"].replace("_", " ").upper()):
+                    for subtask in task["dirs"]:
+                        st.caption("\t\t\t"+subtask["root"].replace("_", " ").upper())
+
 
 class LitStreamlit(L.LightningFlow):
     def configure_layout(self):
-        return frontend.StreamlitFrontend(render_fn=hello_world_streamlit_app)
+        return frontend.StreamlitFrontend(render_fn=home_app)
+
 
 class LitApp(L.LightningFlow):
     def __init__(self):
